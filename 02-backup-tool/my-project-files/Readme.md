66bf6b00b96992a52ed4b53316f06b33c8488c8c
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-06-29 13:49:43 +0530

    add dummy files for the projects

---
+++ 02-backup-tool/my-project-files/Readme.md
@@ -0,0 +1,4 @@
+# My Awesome Project
+
+This is a sample project used for testing the automated backup script.
+It contains a few configuration and source files.
---
+++ 02-backup-tool/my-project-files/src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("This is the main application file.")
+    print("It's very important and needs to be backed up!")
+
+if __name__ == "__main__":
+    main()
